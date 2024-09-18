d7b51c859f5787b5f78cfb1147a7ca786bdaf47b
Kai Hatje <kai.hatje@app-akademie.com>
2024-09-18 15:04:36 +0200

    Removed debug banner.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Simple Beautiful Shopping App',
+      debugShowCheckedModeBanner: false,
       home: MainScreen(),
     );
   }
